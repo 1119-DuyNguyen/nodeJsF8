caed7c3873a2dafb3dcb39ce477b9cde98a24411
Nguyen Thanh Duy <thanhduy191103@gmail.com>
2022-07-28 12:28:49 +0700

    Refactory code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+  const express = require('express')
 const morgan=require('morgan')
 const handlebars=require('express-handlebars')
 const path = require('path')
@@ -6,20 +6,22 @@ const path = require('path')
 const app = express()
 //port muốn website ở cổng nào
 const port = 3000
-
+const route=require('./routes/index.js')
+//template handlebars
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'./resources/views'));
 //http logs
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 //thư mục tĩnh
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))// dùng cho submit html
+app.use(express.json()); //submit js -xml http request, fetch,axios,...
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-//get = route tuyến đường
-//port localhost : 127.0.0.1
-app.listen(port, () => {
+//routes init 
+route(app);
+
+ //port localhost : 127.0.0.1
+ app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
