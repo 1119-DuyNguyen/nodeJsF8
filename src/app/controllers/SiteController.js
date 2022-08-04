7fb4663299b76bd0e64c296b22bdf07971df8901
Nguyen Thanh Duy <thanhduy191103@gmail.com>
2022-08-05 02:22:06 +0700

    add CRUD -soft delete- recycle

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,18 @@
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     //[GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                //vì handlebars kh cho tác động object có function- prototype nên phải chuyển về object thường
+
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch((err) => next(err));
+        // res.render('home');
     }
     //[GET] /search
 
--- src/index.js
+++ src/index.js
@@ -2,26 +2,45 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const methodOverride = require('method-override');
+//import mongoose
+const db = require('./config/db'); // /index.js là mặc định
+db.connect();
 //giống import thư viện và lưu vào biến express
 const app = express();
 //port muốn website ở cổng nào
 const port = 3000;
 const route = require('./routes/index.js');
 //template handlebars
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+            bar() {
+                return 'BAR!';
+            },
+        },
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 //http logs
 //app.use(morgan('combined'))
 //thư mục tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
+
+//để body / res có dữ liệu thì 2 dòng dưới
 app.use(express.urlencoded({ extended: true })); // dùng cho submit html
 app.use(express.json()); //submit js -xml http request, fetch,axios,...
 
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('_method'));
 //routes init
 route(app);
 
 //port localhost : 127.0.0.1
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`App listening on port http://localhost:${port}`);
 });
